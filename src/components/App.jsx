6945adcb95e3bdf549c98ebc093ef683287ac4ad
Bohdan Lytvyniuk <lytvyniukbohdan@gmail.com>
2023-08-17 12:03:30 +0200

    ref. App to hooks

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,32 +1,24 @@
 import Searchbar from './Searchbar/Searchbar';
-import React from 'react';
+import { useEffect, useState } from 'react';
 import ImageGallery from './ImageGallery/ImageGallery';
-// import { ImageGalleryItem } from './ImageGalleryItem/ImageGalleryItem';
-export class App extends React.Component {
-  state = {
-    searchQuery: '',
-   
+
+export function App() {
+  const [searchQuery, setSearchQuery] = useState('');
+
+  const handleSearchBarSubmit = searchQuery => {
+    setSearchQuery(searchQuery);
   };
 
-  
-
-  handleSearchBarSubmit = searchQuery => {
-    this.setState({ searchQuery });
+  const appStyles = {
+    display: 'grid',
+    gridTemplateColumns: '1fr',
+    gridGap: '16px',
+    paddingBottom: '24px',
   };
-
-  render() {
-    const appStyles = {
-      display: 'grid',
-      gridTemplateColumns: '1fr',
-      gridGap: '16px',
-      paddingBottom: '24px',
-    };
-    return (
-      <div style={appStyles}>
-        <Searchbar onSubmit={this.handleSearchBarSubmit} />
-        <ImageGallery query={this.state.searchQuery} />
-      
-      </div>
-    );
-  }
+  return (
+    <div style={appStyles}>
+      <Searchbar onSubmit={handleSearchBarSubmit} />
+      <ImageGallery query={searchQuery} />
+    </div>
+  );
 }
