6a28b030d58e2ac06d198c576f870c612fb504e2
Bohdan Lytvyniuk <lytvyniukbohdan@gmail.com>
2023-08-17 20:51:06 +0200

    ref imageGallery

--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -1,90 +1,95 @@
 import { ImageGalleryItem } from 'components/ImageGalleryItem/ImageGalleryItem';
 import './ImageGallery.css';
-import React from 'react';
+import { useEffect, useState } from 'react';
 import { getFetch } from 'services/getFetch';
 import Button from 'components/Button/Button';
 import { Loader } from 'components/Loader/Loader';
 import PropTypes from 'prop-types';
 
-class ImageGallery extends React.Component {
-  state = {
-    hits: [],
-    totalHits: 0,
-    error: null,
-    status: 'idle',
-    page: 1,
-  };
+function ImageGallery({ query }) {
+  const [hits, setHits] = useState([]);
+  const [totalHits, setTotalHits] = useState(0);
+  const [error, setError] = useState(null);
+  const [status, setStatus] = useState('idle');
+  const [page, setPage] = useState(1);
 
-  async componentDidUpdate(prevProps, prevState) {
-    const prevQuery = prevProps.query;
-    const newQuery = this.props.query;
-    const prevPage = prevState.page;
-    const nextPage = this.state.page;
-    if (newQuery !== prevQuery)
-      try {
-        this.setState({ status: 'pending' ,page: 1});
-        const { hits, totalHits } = await getFetch(newQuery, nextPage);
-        if (totalHits === 0) {
-          throw new Error(`No results found for your query "${newQuery}"`);
-        }
-        this.setState({ hits, totalHits, status: 'resolved' });
-      } catch (error) {
-        this.setState({ error, status: 'rejected' });
+  useEffect(() => {
+    const fetchData = async () => {
+      if (query === '') {
+        return;
       }
-    if (prevPage !== nextPage) {
-      const { hits } = await getFetch(newQuery, nextPage);
-      this.setState({ hits: [...prevState.hits, ...hits] });
-    }
-  }
+      setStatus('pending');
+      setPage(1);
+      try {
+        const { hits, totalHits } = await getFetch(query, 1);
 
-  handleLoadMoreBtnClick = () => {
-    this.setState(prevState => ({
-      page: prevState.page + 1,
-    }));
-    getFetch();
+        if (totalHits === 0) {
+          throw new Error(`No results found for your query "${query}"`);
+        }
+        setHits(hits);
+        setTotalHits(totalHits);
+        setStatus('resolved');
+      } catch (error) {
+        setError(error);
+        setStatus('rejected');
+      }
+    };
+
+    fetchData();
+  }, [query]);
+
+  useEffect(() => {
+    const loadMoreData = async () => {
+      if (page > 1) {
+        const { hits } = await getFetch(query, page);
+
+        setHits(prevHits => [...prevHits, ...hits]);
+      }
+    };
+
+    loadMoreData();
+  }, [page, query]);
+
+  const handleLoadMoreBtnClick = () => {
+    setPage(prevPage => prevPage + 1);
   };
 
-  render() {
-    const { hits, error, status } = this.state;
-    if (status === 'idle') {
-      return (
-        <div className="preview">
-        
-          <h2>Please enter a search query</h2>
-        </div>
-      );
-    }
-    if (status === 'pending') {
-      return (
-        <div>
-          <Loader />
-          Loading...
-        </div>
-      );
-    }
-    if (status === 'rejected') {
-      return <h1>{error.message}</h1>;
-    }
-    if (status === 'resolved') {
-      return (
-        <div>
-          <ul className="ImageGallery">
-            {hits.map(el=>{
-              return <ImageGalleryItem element={el} key={el.id} />
-            })}
-            
-          </ul>
-          {this.state.page < Math.ceil(this.state.totalHits / 12) && (
-            <Button handleBtnClick={this.handleLoadMoreBtnClick} />
-          )}
-        </div>
-      );
-    }
+  if (status === 'idle') {
+    return (
+      <div className="preview">
+        <h2>Please enter a search query</h2>
+      </div>
+    );
+  }
+  if (status === 'pending') {
+    return (
+      <div>
+        <Loader />
+        Loading...
+      </div>
+    );
+  }
+  if (status === 'rejected') {
+    return <h1>{error.message}</h1>;
+  }
+  if (status === 'resolved') {
+    return (
+      <div>
+        <ul className="ImageGallery">
+          {hits.map(el => {
+            return <ImageGalleryItem element={el} key={el.id} />;
+          })}
+        </ul>
+        {page < Math.ceil(totalHits / 12) && (
+          <Button handleBtnClick={handleLoadMoreBtnClick} />
+        )}
+      </div>
+    );
   }
 }
 
 export default ImageGallery;
 
-ImageGallery.propTypes={
-query:PropTypes.string
-}
+ImageGallery.propTypes = {
+  query: PropTypes.string,
+};
